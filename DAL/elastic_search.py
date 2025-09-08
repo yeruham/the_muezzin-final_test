6850a66467def6c0d895903c604e6d7be1f7aa6c
YeruhamMendelson <y0583207549@gmail.com>
2025-09-08 12:21:09 +0300

    add logging to DAL class

--- DAL/elastic_search.py
+++ DAL/elastic_search.py
@@ -1,17 +1,20 @@
 from elasticsearch import Elasticsearch
+from logger.logger import Logger
+logger = Logger.get_logger()
 
 
 class DALElastic:
 
     def __init__(self, host_name, index_name, mappings = None):
-        host = f"http://{host_name}:9200"
-        self.es = Elasticsearch(host)
+        self.host = f"http://{host_name}:9200"
+        self.es = Elasticsearch(self.host)
         self.index = index_name
         self.mappings = mappings
 
 
     def is_connected(self):
         connected = self.es.ping()
+        logger.info(f"connected to elastic {self.host} - ready for operations")
         return connected
 
 
@@ -19,8 +22,10 @@ class DALElastic:
         if not self.es.indices.exists(index=self.index):
             if self.mappings:
                 self.es.indices.create(index=self.index, mappings=self.mappings)
+                logger.info(f"create index {self.index}")
             else:
                 self.es.indices.create(index=self.index)
+                logger.info(f"index {self.index} already exists")
 
 
     def post_document(self, id, user):
--- logger/logger_config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-
-LOGGER_NAME = os.getenv("LOGGER_NAME", "logger-muezzin")
-LOGGER_ES_HOST = os.getenv("LOGGER_ES_HOST", "http://localhost:9200")
-LOGGER_ES_INDEX = os.getenv("LOGGER_ES_INDEX", "logging-muezzin")

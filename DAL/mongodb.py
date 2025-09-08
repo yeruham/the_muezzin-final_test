bf07d3aad4ae79b80b67e22ded917317648415ae
YeruhamMendelson <y0583207549@gmail.com>
2025-09-08 15:39:43 +0300

    i write comments in DAL/mongodb.py

--- DAL/mongodb.py
+++ DAL/mongodb.py
@@ -1,5 +1,6 @@
 from pymongo import MongoClient
 import gridfs
+from pathlib import Path
 from logger.logger import Logger
 logger = Logger.get_logger()
 
@@ -7,6 +8,10 @@ logger = Logger.get_logger()
 class DALMongo:
 
     def __init__(self, prefix, host, database, collection, user= None, password= None):
+        """ class for fast connect to collection in mongodb.
+            params: The details that make up the mongodb uri,
+            name database and name collection that it doesn't matter if they exist or not.
+             for use first run open_connection method"""
         self.prefix = prefix
         self.host = host
         self.database = database
@@ -18,6 +23,7 @@ class DALMongo:
 
 
     def build_URI(self):
+        """ build uri in accordance with the data """
         if self.user and self.password:
             URI = f"{self.prefix}://{self.user}:{self.password}@{self.host}::27017"
         else:
@@ -27,6 +33,8 @@ class DALMongo:
 
 
     def open_connection(self):
+        """ create MongoClient connection and checks if the connection was successful
+            return bool respectively """
         try:
             self.client = MongoClient(self.URI)
             self.client.admin.command("ping")
@@ -38,34 +46,46 @@ class DALMongo:
             return False
 
 
-    def insert_file(self, path, **kwargs):
+    def insert_file(self, source_path, **kwargs):
+        """ insert file from source_path to collection by using gridfs library.
+            receives path of file for upload him and optional **kwargs to keep with him """
+        path = Path(source_path)
+        if not path.exists():
+            raise FileNotFoundError(f"the path file: {path} does not exists")
         if self.client:
             db = self.client[self.database]
             grid_db = gridfs.GridFS(db, collection=self.collection)
-            with open(path, "br") as f:
+            # open the file and send him to mongodb using grid_db.put method
+            with open(source_path, "br") as f:
                 grid_db.put(f, **kwargs)
 
 
-    def get_file(self, id):
+    def load_file(self, file_id, destination_path, new_file_name):
+        """ load file from mongodb collection by field id using by gridfs library.
+            file_id for identify the desired file, destination path folder and new_file_name for load him. """
+        path = Path(destination_path)
+        if not path.exists():
+            raise FileNotFoundError(f"the path file: {path} does not exists")
         if self.client:
             db = self.client[self.database]
-            fs = gridfs.GridFS(db)
-            for grid_out in fs.find({"id": id}):
-                data = grid_out.read()
-                return data
-
+            fs = gridfs.GridFS(db, collection=self.collection)
+            grid_out =  fs.find_one({"id": file_id})
+            data = grid_out.read()
+            with open(f"{destination_path}\\{new_file_name}", "bw") as f:
+                f.write(data)
 
 
     def close_connection(self):
+        """ close the connection """
         if self.client:
             self.client.close()
+            self.client = None
             logger.info(f"connection to mongodb {self.URI} closed")
+
 
 
 # example
 if __name__ == "__main__":
-    dal_mongo = DALMongo("mongodb", "localhost", "muezzin", "podcast")
+    dal_mongo = DALMongo("mongodb", "localhost", "muezzin", "fs")
     print(dal_mongo.open_connection())
-    data = dal_mongo.get_file("download (1).wav-1979-12-31 23:00:00-2396.81 KB-")
-    with open("C:\\python_data\\new_podcast\\new.wav", "bw") as f:
-        f.write(data)
+    #data = dal_mongo.load_file("download (10).wav-1979-12-31 23:00:00-1326.18 KB-", "C:\\python_data\\new_podcast", "new10.wav")

64cf8f8485f44dfd616bf4f384dce31dda7b551d
YeruhamMendelson <y0583207549@gmail.com>
2025-09-10 09:16:28 +0300

    comments in funcs in text_analysis.py

--- classification/text_analysis.py
+++ classification/text_analysis.py
@@ -1,4 +1,5 @@
 def num_word_in_text(word: str, text: list):
+    """ calculates how many times a word appears in text."""
     num_matches = 0
     for i in range(len(text)):
         if text[i] == word:
@@ -10,6 +11,7 @@ def num_word_in_text(word: str, text: list):
 
 
 def num_sentence_in_text(sentence: str, text: list):
+    """ calculates how many times a sentence appears in text. """
     sentence = sentence.split(" ")
     num_matches = 0
     num_loop = len(text) - len(sentence) + 1
@@ -22,10 +24,15 @@ def num_sentence_in_text(sentence: str, text: list):
 
 
 def num_words_in_text(words: list, text: list):
+    """ gets a list of problematic words to search in another list.
+        return how many times were problematic words detected. """
     num_matches = 0
     for word in words:
+        # if the current word it's a sentence
         if len(word.split(" ")) > 1:
             num_matches += num_sentence_in_text(word, text)
         else:
+            # if the current word it's a single word
             num_matches += num_word_in_text(word, text)
+
     return num_matches
